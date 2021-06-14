3a9f083a9ca246a49a386fd686e1a1e13b1ef116
Kartik V Naik <85819372+knaik1302@users.noreply.github.com>
2021-06-14 12:08:10 +0530

    Update data_logging.py

--- data_logging.py
+++ data_logging.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 14 12:00:08 2021
-
-@author: kartik naik
+"""A receptionist has the duty of noting the details of visitors walking-in to the office. She notes in the register their name, phone number, place coming from and body temperature. In a view to change this pen-paper task to a computer task, use your python programming skills to make a solution for this operation.
+The program should be able to gather the four inputs mentioned earlier. Besides, a database/text file should be created in the local device (computer) with this data. Bring out the best solution! When there are no more entries, any solution as per programmer's choice may be used to terminate the execution of the program.
+Note:
+1. Use of any suitable data structure is entertained
+2. Concepts discussed during 'File Handling' may help to create file in machine with current date-time as file name
 """
 
 import datetime
